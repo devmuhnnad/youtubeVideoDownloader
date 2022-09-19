c463470a2dace2f664c29ae27a7a8a6589b72ff9
unknown <muhnnadhabib@gmail.com>
2022-09-19 12:20:03 +0300

    Accept cors

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const { getVideoFormats } = require("./worker");
 
